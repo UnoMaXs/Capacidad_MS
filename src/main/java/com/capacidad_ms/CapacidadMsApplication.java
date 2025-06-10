d79f53841faab34106041e11fad46aed9ebffb23
pierre.rojas_pragma <pier.rojas@pragma.com.co>
2025-06-10 17:42:00 -0500

    Proyecto base

---
+++ src/main/java/com/capacidad_ms/CapacidadMsApplication.java
@@ -0,0 +1,13 @@
+package com.capacidad_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapacidadMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapacidadMsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/capacidad_ms/CapacidadMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capacidad_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapacidadMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
